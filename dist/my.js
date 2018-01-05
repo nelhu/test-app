47986dc13771d2e296a487457034784c44d0e58a
huxuezi <nelson_xuezi_hu@126.com>
2018-01-05 09:32:17 +0800

    update

--- dist/my.js
+++ dist/my.js
@@ -1,5 +1,52 @@
-var a = {name: 'aaa'};
-
-
-var a = {name: 'bbb'};
-
+var app = angular.module('AngularApp', []);
+		
+		app.directive('emmsEditableDropdown', ['$document', function($document) {
+			
+			return {
+				restrict: 'E',
+				replace: true,
+				scope: {
+					items: '=dropDownList',
+					selected: '=currentSelect',
+				},
+				templateUrl: 'emms-editable-dropdown.html',
+				controller: ['$scope', '$element', function(scope, element) {
+					
+					var inputElement = element[0].children[1];
+					var inputJqElement = angular.element(inputElement);
+					
+					scope.select = function(item) {
+						if (item != 'other') {
+							scope.selected = item;
+							inputJqElement.attr('readonly', 'readonly');
+						} else {
+							// 其他
+							scope.selected = null;
+							inputJqElement.removeAttr('readonly');
+							inputJqElement.addClass('fs');
+							inputElement.focus();
+							inputJqElement.attr('placeholder', '请输入...');
+						}
+					}
+					
+					scope.loseFocus = function() {
+						inputJqElement.removeClass('fs');
+					}
+				}]
+			}
+			
+		}])
+		
+		app.controller('AngularCtrl', function($rootScope, $scope, $http, $document, $timeout, $interval, $window, $rootScope, $log, $parse) {
+			
+			$scope.one = "mike";
+			$scope.test = "test"
+			
+			$scope.list = [{id: '1', name: '111'}, {id: '2', name: '222'}];
+			$scope.current;
+			
+			$scope.alert = function() {
+				alert('666');
+			}
+			
+		});
